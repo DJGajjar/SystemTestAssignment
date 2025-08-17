77c21ce6142dde577b10519a7d3bce773a35eeca
Darshan <jolapara25@gmail.com>
2025-08-17 13:19:40 +0530

    Add pod Amplify and AmplifyPlugins/AWSCognitoAuthPlugin

---
+++ AWSAssignment/Pods/Target Support Files/Pods-AWSAssignment/Pods-AWSAssignment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWSAssignment : NSObject
+@end
+@implementation PodsDummy_Pods_AWSAssignment
+@end
---
+++ AWSAssignment/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsassignmentc190fd6a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
